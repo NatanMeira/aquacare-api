8065b5000188bb6697f0ed476445bbe316af7567
Natan Meira <natan.meira@redfox.tech>
2021-08-26 21:56:23 -0300

    feat: alter devices table

--- database/migrations/1629852453959_devices.ts
+++ database/migrations/1629852453959_devices.ts
@@ -7,6 +7,12 @@ export default class Devices extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
+      table
+        .integer('aquarium_id')
+        .unsigned()
+        .references('id')
+        .inTable('aquariums')
+        .onDelete('CASCADE')
       table.string('device_id', 255)
     })
   }
