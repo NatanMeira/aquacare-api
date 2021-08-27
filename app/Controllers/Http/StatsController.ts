230f242a27359a6300e73b4010c623838c6484cb
Natan Meira <natan.meira@redfox.tech>
2021-08-26 21:59:14 -0300

    refactor: add store stats

--- app/Controllers/Http/StatsController.ts
+++ app/Controllers/Http/StatsController.ts
@@ -1,24 +1,45 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Stats from 'App/Models/Stats'
 import CreateStatsValidator from 'App/Validators/Stats/CreateStatsValidator'
+import { AmoniaStats } from 'App/Enum/AmoniaStats.enum'
+import Device from 'App/Models/Device'
 
 export default class StatsController {
-  public async index({}: HttpContextContract) {
-    return Stats.all()
-  }
-
   public async store({ request }: HttpContextContract) {
-    const statsData = await request.validate(CreateStatsValidator)
-    return Stats.create(statsData)
-  }
-
-  public async show({ params }: HttpContextContract) {
-    return Stats.findOrFail(params.id)
+    const requestData = await request.validate(CreateStatsValidator)
+    const device = await Device.findByOrFail('device_id', requestData.device_id)
+    const stats = new Stats()
+    stats.fill({
+      is_habitable: StatsController.verifyIsHabitable(requestData.amonia),
+      aquariumId: device.aquariumId,
+      amonia: requestData.amonia,
+    })
+    return stats.save()
   }
 
   public async destroy({ params }: HttpContextContract) {
     const stats = await Stats.findOrFail(params.id)
     await stats.delete()
     return stats
+  }
+
+  private static verifyIsHabitable(stats: AmoniaStats): boolean {
+    switch (stats) {
+      case AmoniaStats.SAFE:
+        // TODO send notification
+        return true
+
+      case AmoniaStats.ALERT:
+        // TODO send notification
+        return true
+
+      case AmoniaStats.ALARM:
+        // TODO send notification
+        return true
+
+      case AmoniaStats.TOXIC:
+        // TODO send notification
+        return false
+    }
   }
 }
